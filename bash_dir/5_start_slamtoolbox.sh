1f884bad029dce5c8c9f524ca97a5965554a4554
YakiFrog <Frog7352@icloud.com>
2024-12-02 16:24:01 +0900

    planner_serverのプラグインを変更する前のコミット

--- bash_dir/5_start_slamtoolbox.sh
+++ bash_dir/5_start_slamtoolbox.sh
@@ -5,5 +5,5 @@ export ROS_DOMAIN_ID=57
 while : ;do
     read -p "Press [Enter] key to start slamtoolbox..."
     source install/setup.bash
-    ros2 launch slam_toolbox online_async_launch.py
+    ros2 launch slam_toolbox online_async_launch.py use_sim_time:=false slam_params_file:=/home/sirius24/sirius_ws/params/mapper_params_online_async.yaml
 done
