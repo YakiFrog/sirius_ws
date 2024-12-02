677b46ea8addd43d895ac3130d8428ae5436fd08
YakiFrog <Frog7352@icloud.com>
2024-12-02 16:24:01 +0900

    planner_serverのプラグインを変更する前のコミット

--- bash_dir/7_start_nav2_bringup.sh
+++ bash_dir/7_start_nav2_bringup.sh
@@ -7,5 +7,5 @@ while : ;do
     source install/setup.bash
     echo "Input map name (without .yaml): "
     read map_name
-    ros2 launch nav2_bringup bringup_launch.py use_sim_time:=False map:=/home/sirius24/sirius_ws/map_waypoints/map/$map_name.yaml params_file:=/home/sirius24/sirius_ws/src/navigation2/nav2_bringup/params/nav2_params.yaml 
+    ros2 launch nav2_bringup bringup_launch.py use_sim_time:=False map:=/home/sirius24/sirius_ws/map_waypoints/map/$map_name.yaml params_file:=/home/sirius24/sirius_ws/params/nav2_params.yaml
 done
