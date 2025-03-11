47d3061c9b011f4a9bc6f9038b2bd313bc0d1159
YakiFrog <Frog7352@icloud.com>
2025-03-11 21:09:32 +0900

    いいパラメタ．前回のほうがいいかもしれんけど．旋回半径を０か０．１０にすべきかわからん

--- bash_dir/1_start_tf2static_1.sh
+++ bash_dir/1_start_tf2static_1.sh
@@ -6,6 +6,6 @@ export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 while : ;do
     read -p "Press [Enter] key to start tf2 static transform publisher 1..."
     source install/setup.bash
-    ros2 run tf2_ros static_transform_publisher 0.3 0 0.165 0 0 0 base_footprint laser
-    # ros2 run tf2_ros static_transform_publisher 0.3 0 0.83 0 0 0 base_footprint laser
+    # ros2 run tf2_ros static_transform_publisher 0.3 0 0.165 0 0 0 base_footprint laser
+    ros2 run tf2_ros static_transform_publisher 0.3 0 0.365 0 0 0 base_footprint camera_link
 done
