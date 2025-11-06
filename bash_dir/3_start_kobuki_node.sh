4b8ed2c8ded847e95b74207c421f70404d568398
YakiFrog <Frog7352@icloud.com>
2025-11-06 11:28:21 +0900

    ekf仕様のNav2等のパラメタ

--- bash_dir/3_start_kobuki_node.sh
+++ bash_dir/3_start_kobuki_node.sh
@@ -13,8 +13,15 @@ trap 'echo ""; echo "Ctrl + Cが押されましたが、ウィンドウは閉じ
 cd ~/roboteq_ws
 export ROS_DOMAIN_ID=57
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-while : ;do
+while : ; do
     read -p "Press [Enter] key to start roboteq node..."
+    # Ask whether to publish TF. Default: enable (true) when user just presses Enter.
+    read -p "TFを有効にしますか？ (Y/n) デフォルト: Y -> " tf_choice
+    if [ -z "${tf_choice}" ] || [[ "${tf_choice}" =~ ^[Yy] ]]; then
+        pub_tf=true
+    else
+        pub_tf=false
+    fi
     source install/setup.bash
-    ros2 launch roboteq_ros2_driver roboteq_ros2_driver.launch.py 
+    ros2 launch roboteq_ros2_driver roboteq_ros2_driver.launch.py pub_odom_tf:=${pub_tf}
 done
